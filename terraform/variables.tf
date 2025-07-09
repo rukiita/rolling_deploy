e18b797c9a3f846c943f5f3287a58161a3611c7f
lukiita <160990087+rukiita@users.noreply.github.com>
2025-07-09 22:51:17 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,14 +23,14 @@ variable "vpc_cidr_block" {
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets in different AZs"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"] # AZごとに設定
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 # プライベートサブネットの CIDR ブロック (複数のAZに対応)
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets in different AZs"
   type        = list(string)
-  default     = ["10.0.11.0/24", "10.0.12.0/24"] # AZごとに設定
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 # ECS タスクの CPU (Fargate)
@@ -58,47 +58,46 @@ variable "ecr_repository_name" {
 variable "github_owner" {
   description = "GitHub repository owner"
   type        = string
-  default     = "rukiita" # あなたのGitHubユーザー名
+  default     = "rukiita"
 }
 
 variable "github_repo" {
   description = "GitHub repository name"
   type        = string
-  default     = "rolling_deploy" # あなたのGitHubリポジトリ名
+  default     = "rolling_deploy"
 }
 
 # GitHub 接続 ARN
 variable "codeconnections_connection_arn" {
   description = "ARN of the AWS CodeConnections connection to GitHub"
   type        = string
-  default     = "arn:aws:codeconnections:ap-northeast-1:267375937715:connection/86a1b018-5eaf-48f0-944c-858c65d6e38c" # あなたのARNに置き換えてください
+  default     = "arn:aws:codeconnections:ap-northeast-1:267375937715:connection/86a1b018-5eaf-48f0-944c-858c65d6e38c" 
 }
 
 # CodePipeline のアーティファクトS3バケット名
 variable "pipeline_artifact_s3_bucket_name" {
   description = "Name of the S3 bucket for CodePipeline artifacts"
   type        = string
-  default     = "your-unique-pipeline-artifacts-bucket-name-12345" # 必ずユニークなバケット名に置き換えてください
+  default     = "your-unique-pipeline-artifacts-bucket-name-12345"
 }
 
 # Docker Hub のシークレット名 (Secrets Manager)
 variable "dockerhub_secret_name" {
   description = "Name of the Secrets Manager secret for Docker Hub credentials"
   type        = string
-  default     = "dockerDev" # あなたのSecrets Managerシークレット名
+  default     = "dockerDev"
 }
 
 variable "dockerhub_username" {
   description = "Docker Hub username for secrets manager"
   type        = string
-  sensitive   = true # この変数が機密情報であることを示し、Terraformのログ出力などを抑制
+  sensitive   = true
 }
 
 variable "dockerhub_password" {
   description = "Docker Hub password for secrets manager"
   type        = string
-  sensitive   = true # この変数が機密情報であることを示し、Terraformのログ出力などを抑制
-}
+  sensitive   = true
 
 # アベイラビリティゾーンのリスト
 data "aws_availability_zones" "available" {
